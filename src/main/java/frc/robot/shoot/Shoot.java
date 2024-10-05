2cdda65e408d82c06bdc720585c96007e7968612
HENRYTHEGR <henryj.winchester@outlook.com>
2024-10-05 11:40:12 -0400

    made configs

--- src/main/java/frc/robot/shoot/Shoot.java
+++ src/main/java/frc/robot/shoot/Shoot.java
@@ -1,5 +1,41 @@
 package frc.robot.shoot;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 public class Shoot {
-    
+    private final TalonFX lead = new TalonFX(0);
+    private final TalonFX follow = new TalonFX(1);
+
+    private TalonFXConfiguration leadMotorConfig() {
+        var cfg = new TalonFXConfiguration();
+        
+        cfg.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+
+        cfg.Slot0.kP = 0.1;
+        cfg.Slot0.kI = 0.0;
+        cfg.Slot0.kD = 0.0;
+
+
+        return cfg;
+    }
+
+    private TalonFXConfiguration followMotorConfig() {
+        var cfg = new TalonFXConfiguration();
+
+        cfg.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+
+        cfg.Slot0.kP = 0.1;
+        cfg.Slot0.kI = 0.0;
+        cfg.Slot0.kD = 0.0;
+        
+
+        return cfg;
+
+
+    }
 }
